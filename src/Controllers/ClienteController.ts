2129db77aa54daf1f16b663df09ec0555af5909c
Jose-Victor <josevitorppessoa@gmail.com>
2022-12-17 22:56:57 -0300

    Update cliente feito (na gambiarra). delete ta com algum problema

--- src/Controllers/ClienteController.ts
+++ src/Controllers/ClienteController.ts
@@ -1,3 +1,5 @@
+import { Cliente } from './../entities/Cliente';
+import { Colaborador } from './../entities/Colaborador';
 import { colaboradorRepository } from './../repositories/colaboradorRepository';
 import { clienteRepository } from './../repositories/clienteRepository';
 import { Request, Response } from "express";
@@ -45,7 +47,78 @@ export class ClienteController {
             message: 'internal server error'
         })
     }
-}
+  }
+
+  async atualizarCliente(req: Request, res: Response) { // não ta pegando ainda
+    const { nomeCompleto, Cpf, isAtivo} = req.body;
+    const { idCliente } = req.params
+    const id = idCliente
+    console.log({ nomeCompleto, Cpf, isAtivo });
+
+    try {
+
+      const cliente = await clienteRepository.findOneBy({ id: idCliente })
+      console.log("encontrou o cliente");
+      
+
+      if(!cliente) {
+        return res.status(404).json({message: "Cliente não existe"})
+      }
+
+      // const colaborador = await colaboradorRepository.findOneBy({ uuid : cliente.colaborador.uuid })
+      // if(!colaborador) {
+      //   return res.status(404).json({message: "Colaborador não existe"})
+      // }
+
+      cliente.nomeCompleto = nomeCompleto ? nomeCompleto : cliente.nomeCompleto
+      cliente.Cpf = Cpf ? Cpf : cliente.Cpf
+      cliente.isAtivo = isAtivo ? isAtivo : cliente.isAtivo
+      console.log("antes do update");
+      
+      
+      const atualizarCliente = await clienteRepository.update({id}, {
+        nomeCompleto,
+        Cpf,
+        isAtivo,
+      })
+
+      await clienteRepository.save(cliente)
+
+      console.log("depois do update");
+      
+      // await clienteRepository.save(cliente)
+      
+      return res.status(204).json({mensagem: "Cliente atualizado"})
+
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({mensagem: 'Ocorreu um erro na Atualização do cliente'})
+    }    
+
+  } 
+
+  async excluirCliente(req: Request, res: Response){
+    const { idCliente } = req.params
+    try{
+      
+        const clienteSendoExcluido = await clienteRepository.findOneBy({ id: idCliente})
+        if (!clienteSendoExcluido) {
+          return res.json({message: "cliente inexistente"})
+        }
+        await clienteRepository.delete(clienteSendoExcluido.id)
+
+        return res.status(200)
+    }   catch (error) {
+        console.log(error);
+        
+        return res.status(500).json({
+            error: error,
+            message: 'internal server error, erro ao deletar cliente'
+
+        })
+    }
+  }
+  
 
 
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,14 @@
-import { clienteRepository } from './repositories/clienteRepository';
 import { Router } from "express";
 import { ClienteController } from "./Controllers/ClienteController";
 import { ColaboradorController } from "./Controllers/ColaboradorController";
 
 const routes = Router()
 
+
 routes.post('/cadastrarcliente/:idcolaborador', new ClienteController().cadastrarCliente)
 routes.get('/listarclientes', new ClienteController().listarClientes)
+routes.put('/atualizarcliente/:idcliente', new ClienteController().atualizarCliente)
+routes.delete('/excluircliente/:idcliente', new ClienteController().excluirCliente)
 
 
 routes.post('/cadastrarcolaborador', new ColaboradorController().create)
